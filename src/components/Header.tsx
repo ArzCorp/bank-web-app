7fa7dabca5771152ad397e649bb2d3fb8b083040
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-06-24 18:29:07 -0500

    ✨ added container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,20 @@
 import Image from 'next/image'
+import Container from './Container'
 
 export default function Header() {
 	return (
 		<header className="header">
-			<div></div>
-			<h2 className="header__username">Hola Osvaldo</h2>
-			<Image
-				className="header__userphoto"
-				src="/img/user.jpeg"
-				alt="foto osvaldo"
-				width={24}
-				height={24}
-			/>
+			<Container>
+				<div></div>
+				<h2 className="header__username">Hola Osvaldo</h2>
+				<Image
+					className="header__userphoto"
+					src="/img/user.jpeg"
+					alt="foto osvaldo"
+					width={24}
+					height={24}
+				/>
+			</Container>
 		</header>
 	)
 }
